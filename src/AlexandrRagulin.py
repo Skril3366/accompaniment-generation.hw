1be19531603eb850c57bc78b6b3e218cf4fe79b2
Alexandr Ragulin <klodiys62@gmail.com>
2022-11-29 23:06:51 +0300

    concatenate to one file and rename it

--- src/AlexandrRagulin.py
+++ src/AlexandrRagulin.py
@@ -10,7 +10,25 @@ import argparse
 import mido
 import music21
 
-from errors import NoSetTempoMessageFound, NoTimeSignatureMessageFound
+
+# ------------------------ Errors  --------------------------------
+
+class NoteNumberOutOfRange(Exception):
+    """
+    Raised when note number is out of range [0, 127]
+    """
+
+
+class NoSetTempoMessageFound(Exception):
+    """
+    Raised when no set tempo message found while processing midi file
+    """
+
+
+class NoTimeSignatureMessageFound(Exception):
+    """
+    Raised when no time signature message found while processing midi file
+    """
 
 # ------------------------ Composition parsing --------------------------------
 
